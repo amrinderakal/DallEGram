29bfb37ccb76b982595740e48a357f6799255d18
Pavan Patel <platinumpavan123@gmail.com>
2023-09-28 14:50:44 -0400

    Created intial file structures

---
+++ backend/index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+const port = 8000
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
+
+// control c to stop
+// npx nodemon index.js
