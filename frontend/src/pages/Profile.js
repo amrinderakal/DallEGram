14434cb2a459a94993f4b914f74a720b1ad3939b
Shehneel <shehneelashraf@gmail.com>
2023-10-30 12:49:27 -0400

    designed basic profile page

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -8,6 +8,10 @@ import {
   Nav,
   Navbar,
   NavDropdown,
+  Col,
+  Image, 
+  Row,
+  Table
 } from "react-bootstrap";
 import { useAuth } from "../context/AuthContext";
 import NavigationBar from "../components/NavigationBar";
@@ -55,23 +59,64 @@ export default function Homepage() {
 
       {/* Greeting User (text) */}
       <Container
-        className="d-flex flex-column align-items-center justify-content-center"
+        // className="d-flex flex-column align-items-center justify-content-center"
+        className="d-flex flex-column"
         style={{ minHeight: "20vh" }}
       >
-        {/* change "USER" to specific name */}
-        <div className="w-100" style={{ maxWidth: "700px" }}>
-          <div className="d-flex align-items-center justify-content-center w-100">
-            <h2>Welcome!</h2>
-          </div>
-        </div>
+      
+      <Row>
+        <Col xs={6} style={{marginTop:"6%"}}>
+          <Image
+                className="d-block"
+                thumbnail
+                fluid
+                src={'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png'}
+                alt="Empty Profile Page Image"
+                width="37%"
+                height="37%"
+                style={{ marginLeft: '20%'}}
+            />
+        </Col>
+        <Col xs={6} style={{marginTop:"6%", marginLeft:"-20%"}}>
+          <h1 style={{textAlign:"left"}}>
+            Johnny Appleseed
+          </h1>
+          <p style={{textAlign:"left"}}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+          Pellentesque sit amet aliquam lorem. Donec molestie eleifend augue vitae congue. 
+          Nulla maximus imperdiet feugiat. Vivamus suscipit tortor non velit vulputate, ac hendrerit neque dignissim. 
+          Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. 
+          Vivamus vel facilisis magna. Integer vel vestibulum libero. Nullam turpis mi, egestas tempor lacus sit amet, lacinia fermentum ante. 
+          Ut eleifend vitae sem vel aliquam. Mauris sed maximus arcu. Nulla risus odio, interdum a massa id, dictum pretium diam.
+          </p>
+        </Col>
+      </Row>
 
-        <div className="w-100" style={{ maxWidth: "700px" }}>
-          <div className="d-flex align-items-center justify-content-center w-100">
-            <h4>Let's look at some of the images you've created.</h4>
-          </div>
-        </div>
+      <Row className="d-flex">
+        <Table bordered size="sm" style={{width:"15%", marginLeft: '12%', textAlign:"center"}}>
+          <thead>
+            <tr>
+              <th>Followers</th>
+              <th>Following</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>300</td>
+              <td>250</td>
+            </tr>
+          </tbody>
+        </Table>
+      </Row>
 
-        {/* Gallery of images */}
+      <Row className="d-flex flex-column mx-auto" style={{marginTop:"-2%"}}>
+        <Col>
+          <h2>
+            <b><u>Your Creations</u></b>
+          </h2>
+        </Col>
+      </Row>
+
       </Container>
     </>
   );
