bd473aaaa518a156e0c49f955612e3b6512e4d27
Shehneel <shehneelashraf@gmail.com>
2023-11-27 15:28:13 -0500

    tried open api

--- frontend/src/components/ImageGenerator.js
+++ frontend/src/components/ImageGenerator.js
@@ -1,8 +1,34 @@
 import React, { useState } from "react";
-import { Form, Button, FloatingLabel, Container, Col, Row } from "react-bootstrap";
+import { Form, Button, Col, Row, Container } from "react-bootstrap";
+import OpenAI from 'openai';
 
 function ImageGenerator() {
   const [img_desc, setImageDesc] = useState("");
+  const [result, setResult] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const openai = new OpenAI({
+    apiKey: process.env.REACT_APP_API_KEY,
+    dangerouslyAllowBrowser: true, // IDK ABOUT THIS 
+  });
+
+  const handleGenerate = async () => {
+    setLoading(true);
+
+    try {
+      const res = await openai.images.generate({
+        prompt: img_desc,
+        n: 1,
+        size: "256x256",
+      });
+
+      setLoading(false);
+      setResult(res.data.data[0].url);
+    } catch (error) {
+      setLoading(false);
+      console.error("Error generating image:", error.message);
+    }
+  };
 
   return (
     <>
@@ -11,55 +37,51 @@ function ImageGenerator() {
        .btn-primary {
          background-color: #007AAD;
          color: white;
-         width: 120%;
-         height: 80%; // Set button height to 100%
+         width: 60%;
+         height: 50px;
        }    
        .btn-primary:hover {
          background-color: #005071;
          color: white;
        }
-        @media (max-width: 993px) {
-          .justify-content-center {
-            text-align: center;
-          }
-        }
     `}
       </style>
 
-      <Row
-        className="d-flex flex-row align-items-stretch justify-content-center mt-1 mb-1"
-        style={{ color: "white" }}
-      >
-        <Col
-          lg={8}
-          className="d-flex flex-column align-items-center justify-content-top mt-1 mb-1"
-          style={{ marginLeft: "200px" }}
-        >
-          <Form className="w-100">
-            <Form.Group id="img-description" className="mb-3">
-              <FloatingLabel controlId="floatingInput" label="Description">
+      <Container fluid style={{ marginLeft: "70px" }}>
+        <Row className="mt-1" style={{ color: "white" }}>
+          <Col xs={12} className="mt-1 mb-1">
+            <p>Start with a detailed description</p>
+          </Col>
+          <Col xs={8} md={8} className="mt-1 mb-1">
+            <Form>
+              <Form.Group id="img-description" className="mb-3">
                 <Form.Control
-                  type="img_desc"
+                  type="text"
                   value={img_desc}
                   onChange={(e) => setImageDesc(e.target.value)}
                   required
-                  style={{ borderColor: "#000000", width: "100%" }} // Make the search bar wider
-                  placeholder="Description"
+                  style={{ borderColor: "#000000", width: "100%", height: "50px" }}
+                  placeholder="An armchair in the shape of an avocado..."
                 />
-              </FloatingLabel>
-            </Form.Group>
-          </Form>
-        </Col>
-        <Col
-          className="d-flex flex-column align-items-center justify-content-top mt-1 mb-1"
-          style={{ marginRight: "12%"}}
-        >
-          {/* onClick={handleGenerate} */}
-          <Button variant="primary" >
-            Generate
-          </Button>
-        </Col>
-      </Row>
+              </Form.Group>
+            </Form>
+          </Col>
+          <Col xs={12} md={4} className="mt-1 mb-1">
+            <Button variant="primary" onClick={handleGenerate} >
+              Generate
+            </Button>
+          </Col>
+        </Row>
+      </Container>
+
+      {/* Display result image if available */}
+      {result && (
+        <Row className="justify-content-center mt-1 mb-1">
+          <Col lg={8} className="mt-1 mb-1">
+            <img className="result-image" src={result} alt="result" style={{ width: "100%" }} />
+          </Col>
+        </Row>
+      )}
     </>
   );
 }
