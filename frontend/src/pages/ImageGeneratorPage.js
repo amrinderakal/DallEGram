86fafe1519296d5e7642227e8ac6422da959d106
Shehneel <shehneelashraf@gmail.com>
2023-11-27 19:44:02 -0500

    added example images

--- frontend/src/pages/ImageGeneratorPage.js
+++ frontend/src/pages/ImageGeneratorPage.js
@@ -1,7 +1,10 @@
 import React from "react";
-import { Col, Row, Image } from "react-bootstrap";
+import { Col, Row, Image, Container} from "react-bootstrap";
 import ImageGenerator from "../components/ImageGenerator";
 import NavigationBar from "../components/NavigationBar";
+import example1 from "../assets/example1.png";
+import example2 from "../assets/example2.png";
+import example3 from "../assets/example3.png";
 
 export default function ImageGeneratorPage() {
   return (
@@ -19,6 +22,51 @@ export default function ImageGeneratorPage() {
       >
 
         <ImageGenerator/>
+
+          <Row
+            className="d-flex flex-column align-items-center justify-content-center mt-3"
+            style={{ color: "white" }}
+          >
+            <Col style={{ marginTop: "20px", textAlign: "center" }}>
+              <h2 style={{ fontSize: "1.5rem" }}>
+                <b>
+                  <u>Generated Images Examples</u>
+                </b>
+              </h2>
+            </Col>
+          </Row> 
+
+        <Container fluid>
+         <Row
+           className="d-flex flex-row align-items-center justify-content-center"
+           style={{ color: "white"}}
+         >
+           <Col className="d-flex justify-content-center">
+             <Image
+               className="d-block"
+               thumbnail
+               src={example1}
+               style={{ width: "50%", height: "50%" }} 
+             />
+           </Col>
+           <Col className="d-flex justify-content-center">
+             <Image
+               className="d-block"
+               thumbnail
+               src={example2}
+               style={{ width: "50%", height: "50%" }} 
+             />
+           </Col>
+           <Col className="d-flex justify-content-center">
+             <Image
+               className="d-block"
+               thumbnail
+               src={example3}
+               style={{ width: "50%", height: "50%" }} 
+             />
+           </Col>
+         </Row> 
+         </Container>
 
       </div>
     </>
