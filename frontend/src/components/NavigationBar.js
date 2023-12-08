83be4e7668a09a124523b36d959223ce3a32a1e1
aman <102567339+comeonaman@users.noreply.github.com>
2023-12-08 14:46:52 -0500

    fixed navbar responsiveness

--- frontend/src/components/NavigationBar.js
+++ frontend/src/components/NavigationBar.js
@@ -15,30 +15,24 @@ function NavigationBar() {
   const { logout } = useAuth();
 
   const navbarStyle = {
-    backgroundColor: "#383838", // Your desired hex color
+    backgroundColor: "#fff", // Your desired hex color
   };
 
   return (
-    <Navbar expand="lg" className="bg-body-tertiary" style={navbarStyle}>
+    <Navbar collapseOnSelect expand="lg" style={navbarStyle} variant="light">
       <Container fluid>
-        <Navbar.Brand>
-          <Nav.Link href="/homepage">
-            <img
-              src={Logo}
-              alt="DallEGram Logo"
-              width="125"
-              height="30"
-              className="d-inline-block align-top"
-            />
-          </Nav.Link>
+        <Navbar.Brand href="/homepage">
+          <img
+            src={Logo}
+            alt="DallEGram Logo"
+            width="125"
+            height="30"
+            className="d-inline-block align-top"
+          />
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="ms-auto my-2 my-lg-0" // spacing ms-auto: right, me-auto: left
-            style={{ maxHeight: "100px" }}
-            navbarScroll
-          >
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="ms-auto">
             <Nav.Link href="/homepage">Public Feed</Nav.Link>
             <Nav.Link href="/image-generator-page">Image Generator</Nav.Link>
             <Nav.Link href="/profile">Profile</Nav.Link>
