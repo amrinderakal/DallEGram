2b5e1b71ceb7e5430b31e39ac1846ceb71eefc9f
aman <102567339+comeonaman@users.noreply.github.com>
2023-12-08 15:15:57 -0500

    download button for generated image

--- frontend/src/components/ImageGenerator.js
+++ frontend/src/components/ImageGenerator.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Form, Button, Col, Row, Container, Image} from "react-bootstrap";
+import { Form, Button, Col, Row, Container, Image } from "react-bootstrap";
 import OpenAI from "openai";
 import Modal_ImgGen from "../components/Modal_ImgGen";
 import { useAuth } from "../context/AuthContext";
@@ -8,7 +8,7 @@ import { useDatabase } from "../context/DatabaseContext";
 function ImageGenerator() {
   const { currentUser } = useAuth();
   const [isLoading, setIsLoading] = useState(false);
-  const [caption, setCaption] = useState(""); 
+  const [caption, setCaption] = useState("");
   const { user } = useDatabase();
 
   const [img_desc, setImageDesc] = useState("");
@@ -31,40 +31,42 @@ function ImageGenerator() {
         n: 1,
         size: "256x256",
       });
-  
+
       setLoading(false);
       setResult(res.data[0].url);
       // handleShow();
-  
     } catch (error) {
       setLoading(false);
       console.error("Error generating image:", error.message);
     }
-  };  
+  };
 
   //const handlePostButtonClick = () => {
-    //setShowModal(true);
+  //setShowModal(true);
   //};
   const handlePostButtonClick = async (result, currentUser, user, caption) => {
-    setShowModal(false); 
-  
+    setShowModal(false);
+
     try {
       setIsLoading(true);
-  
-      const response = await fetch("http://localhost:8000/add_image_to_feed_collection", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          uid: currentUser.uid,
-          imageURL: result,
-          caption: caption,
-          username: user.username,
-          profilePic: user.profilePic,
-        }),
-      });
-  
+
+      const response = await fetch(
+        "http://localhost:8000/add_image_to_feed_collection",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            uid: currentUser.uid,
+            imageURL: result,
+            caption: caption,
+            username: user.username,
+            profilePic: user.profilePic,
+          }),
+        }
+      );
+
       if (response.ok) {
         console.log("Image added to the feed");
       } else {
@@ -76,6 +78,15 @@ function ImageGenerator() {
       setIsLoading(false);
     }
   };
+
+  const handleDownload = () => {
+    const link = document.createElement("a");
+    link.href = result;
+    link.target = "_blank";
+    link.download = "generated_image.png";
+    link.click();
+  };
+
   return (
     <>
       <style type="text/css">
@@ -95,7 +106,7 @@ function ImageGenerator() {
 
       <Container fluid style={{ marginLeft: "70px" }}>
         <Row className="mt-1" style={{ color: "white" }}>
-          <Col xs={12} className="mt-1 mb-1" style={{paddingTop:"60px"}}>
+          <Col xs={12} className="mt-1 mb-1" style={{ paddingTop: "60px" }}>
             <p>Start with a detailed description</p>
           </Col>
           <Col xs={8} md={8} className="mt-1 mb-1">
@@ -117,7 +128,11 @@ function ImageGenerator() {
             </Form>
           </Col>
           <Col xs={12} md={4} className="mt-1 mb-1">
-            <Button variant="primary" onClick={handleGenerate} disabled={loading}>
+            <Button
+              variant="primary"
+              onClick={handleGenerate}
+              disabled={loading}
+            >
               Generate
             </Button>
           </Col>
@@ -126,37 +141,44 @@ function ImageGenerator() {
 
       {/* Display result image if available */}
       {result && (
-      <>
-        <Row className="d-flex flex-row align-items-center justify-content-center">
-          <Col lg={8} className="d-flex justify-content-center">
-            <Image
-              className="result-image"
-              thumbnail
-              src={result}
-              alt="result"
-              style={{ width: "50%", height: "75%" }}
-            />
-          </Col>
-        </Row>
-
-        {/* Post button if image appears */}
-        <Row className="d-flex flex-column align-items-center justify-content-center">
-        <Col lg={8} className="text-center">
-          <Button variant="primary" onClick={() => setShowModal(true)}>
-            Post
-          </Button>
-          <Modal_ImgGen
-            show={showModal}
-            setShow={setShowModal}
-            handlePostButtonClick={handlePostButtonClick}  
-            result={result}
-            currentUser={currentUser}
-            user={user}
-          />
-        </Col>
-      </Row>
-      </>
-    )}
+        <>
+          <Row className="d-flex flex-row align-items-center justify-content-center">
+            <Col lg={8} className="d-flex justify-content-center">
+              <Image
+                className="result-image"
+                thumbnail
+                src={result}
+                alt="result"
+                style={{ width: "50%", height: "75%" }}
+              />
+            </Col>
+          </Row>
+          {/* Download button */}
+          <Row className="d-flex flex-column align-items-center justify-content-center">
+            <Col lg={8} className="text-center">
+              <Button variant="primary" onClick={handleDownload}>
+                Download
+              </Button>
+            </Col>
+          </Row>
+          {/* Post button if image appears */}
+          <Row className="d-flex flex-column align-items-center justify-content-center">
+            <Col lg={8} className="text-center">
+              <Button variant="primary" onClick={() => setShowModal(true)}>
+                Post
+              </Button>
+              <Modal_ImgGen
+                show={showModal}
+                setShow={setShowModal}
+                handlePostButtonClick={handlePostButtonClick}
+                result={result}
+                currentUser={currentUser}
+                user={user}
+              />
+            </Col>
+          </Row>
+        </>
+      )}
     </>
   );
 }
